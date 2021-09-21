e65a058314fef371ba90b57f8bbb43a7e6d25e87
erose <89544258+ethanbrose@users.noreply.github.com>
2021-09-21 12:20:16 -0700

    decompressor + binaryOutput

--- src/Compress.java
+++ src/Compress.java
@@ -7,8 +7,7 @@ public class Compress {
 	private int counter;
 	private FileOutputStream fos;
 	private ArrayList<Integer> outputIntegerCodes;
-	
-	
+
 	private static final int BIT_0 = 0x001;
 	private static final int BIT_1 = 0x002;
 	private static final int BIT_2 = 0x004;
@@ -24,6 +23,7 @@ public class Compress {
 	public Compress(String inputFileName) throws FileNotFoundException {
 		//constructor; throw is needed for the FileReader
 		codeTable = new HashMap<String,Integer>();
+		outputIntegerCodes = new ArrayList<Integer>();
 		//initialize codeTable with Strings of ASCII values starting with space(32) and ending with ~ (126)
 		counter = 0;
 		for (int i=0; i<256; i++) 
@@ -36,7 +36,6 @@ public class Compress {
 		reader = new BufferedReader(new FileReader(inputFile));
 	}
 
-
 	public void encode() throws IOException {
 		int nextLetterInt = 0; 
 		String currString, nextLetter;
@@ -47,7 +46,6 @@ public class Compress {
 			if(codeTable.containsKey(currString + nextLetter)) {
 				System.out.println("repeated: " + currString + nextLetter);
 				currString += nextLetter;
-//				outputIntegerCodes.add(codeTable.get(currString));
 			}
 			else {
 				codeTable.put(currString+nextLetter, counter);
@@ -60,6 +58,27 @@ public class Compress {
 		}
 		outputIntegerCodes.add(codeTable.get(currString));
 		reader.close();
+
+
+		byte[] binaryOutput = new byte [(int) Math.ceil(9*(outputIntegerCodes.size())/8)];
+
+		int currentBitPlace = 0;
+
+		for(int i : outputIntegerCodes) {
+			for(int j =0;j<0;j++) {
+				int theByte=currentBitPlace/8;
+				int offset=currentBitPlace%8;
+				if((i>>j&1)==1) {
+					binaryOutput[theByte]|=1<<offset;
+				}
+			}
+
+			//add all the bits for the ith integer in our output
+
+		}
+
+		System.out.println(outputIntegerCodes);
+		System.out.println(binaryOutput);
 
 		//		File outputFile = new File("EncodedOutput.dat");
 		//		outputFile.createNewFile();
@@ -76,22 +95,45 @@ public class Compress {
 		//		fos.close();
 		// sorry! the binary data is not showing up in the file but it is stored in rawData, for some reason file output stream will not write to the specified file, but it is being populated with something (just not visible). 
 		// the first working commit uses a print writer to output all of the correct integers that represent characters
+
+		File outputFile = new File("outputFile.bin");
+		try (FileOutputStream outputStream = new FileOutputStream(outputFile)) {
+			outputStream.write(binaryOutput);
+		}
 	}
-
-
-
-	public String intToBinary(int num)
-	{
-		return Integer.toBinaryString(num);
+	public static String decompress(List<Integer>inputNums){
+		HashMap <Integer,String> map = new HashMap <Integer,String> ();
+		int counter = 0; //tracks size of array
+		int current = 0;
+		int next = 0;
+		String output = "";
+		String currentWord="";
+		String nextWord="";
+		String theAddition="";
+		int x = inputNums.size(); //the length of the input array
+		
+		for (int i=0; i<256; i++) {
+			map.put(i,""+(char)i);
+			counter++;
+		}
+		for (int a=0;a<x;a++){
+			current=inputNums.get(a);
+			next=inputNums.get(a++);
+			if (next<counter){
+				currentWord=map.get(current);
+				nextWord=map.get(next);
+				theAddition=currentWord+nextWord.substring(0, 1);
+				map.put(counter, theAddition);
+				counter++;
+			}
+			else{
+				currentWord=map.get(current);
+				String letter = currentWord.substring(0,1);
+				theAddition=currentWord+letter;
+				map.put(counter, theAddition);
+				counter++;
+			}
+		}
+		return output;
 	}
-
-
-
-
-
-
-
-
-
-
 }
--- src/LZWTester.java
+++ src/LZWTester.java
@@ -10,53 +10,53 @@ public class LZWTester {
 
 
 		//taken from https://www.geeksforgeeks.org/counting-number-lines-words-characters-paragraphs-text-file-using-java/
-
-		File file = new File(
-				"lzw-file3.txt");
-		FileInputStream fileInputStream
-		= new FileInputStream(file);
-		InputStreamReader inputStreamReader
-		= new InputStreamReader(fileInputStream);
-		BufferedReader bufferedReader
-		= new BufferedReader(inputStreamReader);
-
-		String line;
-		int characterCount = 0;
-
-
-		while ((line = bufferedReader.readLine()) != null) {
-
-			characterCount += line.length();
-			String words[] = line.split("\\s+");
-
-		}
-		System.out.println("Total number of characters from input file = " + characterCount);
-
-
-		File file2 = new File(
-				"EncodedOutput.dat");
-		FileInputStream fileInputStream2
-		= new FileInputStream(file2);
-		InputStreamReader inputStreamReader2
-		= new InputStreamReader(fileInputStream2);
-		BufferedReader bufferedReader2
-		= new BufferedReader(inputStreamReader2);
-
-		String line2;
-		int wordCount = 0;
-
-
-		while ((line2 = bufferedReader2.readLine()) != null) {
-
-
-			String words[] = line2.split("\\s+");
-			wordCount += words.length;
-			// this may be registering new lines as its ascii characters 
-		}
-
-
-		System.out.println("Total word count in output file= "
-				+ wordCount);
-
+//
+//		File file = new File(
+//				"lzw-file3.txt");
+//		FileInputStream fileInputStream
+//		= new FileInputStream(file);
+//		InputStreamReader inputStreamReader
+//		= new InputStreamReader(fileInputStream);
+//		BufferedReader bufferedReader
+//		= new BufferedReader(inputStreamReader);
+//
+//		String line;
+//		int characterCount = 0;
+//
+//
+//		while ((line = bufferedReader.readLine()) != null) {
+//
+//			characterCount += line.length();
+//			String words[] = line.split("\\s+");
+//
+//		}
+//		System.out.println("Total number of characters from input file = " + characterCount);
+//
+//
+//		File file2 = new File(
+//				"EncodedOutput.dat");
+//		FileInputStream fileInputStream2
+//		= new FileInputStream(file2);
+//		InputStreamReader inputStreamReader2
+//		= new InputStreamReader(fileInputStream2);
+//		BufferedReader bufferedReader2
+//		= new BufferedReader(inputStreamReader2);
+//
+//		String line2;
+//		int wordCount = 0;
+//
+//
+//		while ((line2 = bufferedReader2.readLine()) != null) {
+//
+//
+//			String words[] = line2.split("\\s+");
+//			wordCount += words.length;
+//			// this may be registering new lines as its ascii characters 
+//		}
+//
+//
+//		System.out.println("Total word count in output file= "
+//				+ wordCount);
+//
 	}
 }
