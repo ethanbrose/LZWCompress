0233fdc9b71a1f5ec650b324048b5ac5b3b3883c
erose <89544258+ethanbrose@users.noreply.github.com>
2021-09-23 09:09:56 -0700

    Apparently I have changes.

--- src/LZWTester.java
+++ src/LZWTester.java
@@ -3,7 +3,7 @@ import java.io.*;
 public class LZWTester {
 
 	public static void main(String[]args) throws IOException {
-		Compress compressor = new Compress("lzw-file1.txt");
+		Compress compressor = new Compress("lzw-file3.txt");
 		//remember the import statement at the top for BufferedReader and FileReader
 		compressor.encode();
 		//System.out.println("DONE");
